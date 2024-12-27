b3e827bf4f1aa1127ff6024a865d2c188a086902
Amara Santos <yarasousasantos24@gmail.com>
2024-12-27 18:05:59 -0300

    Update in connection with repository and service

--- src/main/java/com/amara/workshopmongo/services/UserService.java
+++ src/main/java/com/amara/workshopmongo/services/UserService.java
@@ -3,10 +3,12 @@ package com.amara.workshopmongo.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.amara.workshopmongo.domain.User;
 import com.amara.workshopmongo.repository.UserRepository;
 
+@Service
 public class UserService {
 	
 	@Autowired
