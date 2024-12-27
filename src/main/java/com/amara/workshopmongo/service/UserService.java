cf358f29b4bab019f5c4e45424160686c4688966
Amara Santos <yarasousasantos24@gmail.com>
2024-12-27 20:03:33 -0300

    Getting user by ID

--- src/main/java/com/amara/workshopmongo/service/UserService.java
+++ src/main/java/com/amara/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
-package com.amara.workshopmongo.services;
+package com.amara.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.amara.workshopmongo.domain.User;
 import com.amara.workshopmongo.repository.UserRepository;
+import com.amara.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,6 +20,9 @@ public class UserService {
 		return repo.findAll();
 	}
 	
-	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 
 }
